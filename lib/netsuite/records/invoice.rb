387221691856509e6e92a9d0829f8fab950f2e0a
maoqiuyun <ily-mqy@163.com>
2024-02-19 15:07:33 +0800

    invoice add shipping_tax_code refs

--- lib/netsuite/records/invoice.rb
+++ lib/netsuite/records/invoice.rb
@@ -16,11 +16,11 @@ module NetSuite
         :deferred_revenue, :discount_amount, :discount_date, :discount_rate,
         :due_date, :email, :end_date, :est_gross_profit, :est_gross_profit_percent, :exchange_rate,
         :exclude_commission, :exp_cost_disc_amount, :exp_cost_disc_print, :exp_cost_disc_rate, :exp_cost_disc_tax_1_amt,
-        :exp_cost_disc_taxable, :exp_cost_discount, :exp_cost_list, :exp_cost_tax_code, :exp_cost_tax_rate_1,
+        :exp_cost_disc_taxable, :exp_cost_discount, :exp_cost_list, :exp_cost_tax_rate_1,
         :exp_cost_tax_rate_2, :fax, :fob, :gift_cert_redemption_list, :handling_tax_1_rate,
-        :handling_tax_2_rate, :handling_tax_code, :is_taxable, :item_cost_disc_amount, :item_cost_disc_print,
+        :handling_tax_2_rate, :is_taxable, :item_cost_disc_amount, :item_cost_disc_print,
         :item_cost_disc_rate, :item_cost_disc_tax_1_amt, :item_cost_disc_taxable, :item_cost_discount, :item_cost_list,
-        :item_cost_tax_code, :item_cost_tax_rate_1, :item_cost_tax_rate_2, :last_modified_date,
+        :item_cost_tax_rate_1, :item_cost_tax_rate_2, :last_modified_date,
         :linked_tracking_numbers, :memo, :message, :message_sel, :on_credit_hold, :opportunity,
         :other_ref_num, :partners_list, :rev_rec_end_date,
         :rev_rec_on_rev_commitment, :rev_rec_schedule, :rev_rec_start_date, :revenue_status, :sales_effective_date,
@@ -28,7 +28,7 @@ module NetSuite
         :shipping_cost, :shipping_tax_1_rate, :shipping_tax_2_rate, :source, :start_date,
         :status, :sync_partner_teams, :sync_sales_teams, :tax_2_total,
         :tax_total, :time_disc_amount, :time_disc_print, :time_disc_rate, :time_disc_tax_1_amt, :time_disc_taxable,
-        :time_discount, :time_list, :time_tax_code, :time_tax_rate_1, :time_tax_rate_2, :to_be_emailed, :to_be_faxed,
+        :time_discount, :time_list, :time_tax_rate_1, :time_tax_rate_2, :to_be_emailed, :to_be_faxed,
         :to_be_printed, :total_cost_estimate, :tracking_numbers, :tran_date, :tran_id, :tran_is_vsoe_bundle,
         :vat_reg_num, :vsoe_auto_calc, :tax_rate
 
@@ -138,7 +138,9 @@ module NetSuite
 
       record_refs :account, :bill_address_list, :custom_form, :department, :entity, :klass, :partner,
                   :posting_period, :ship_address_list, :terms, :location, :sales_rep, :tax_item, :created_from,
-                  :ship_method, :lead_source, :promo_code, :subsidiary, :currency, :approval_status, :job, :discount_item, :shipping_tax_code
+                  :ship_method, :lead_source, :promo_code, :subsidiary, :currency, :approval_status, :job, :discount_item, :shipping_tax_code,
+                  :exp_cost_tax_code, :handling_tax_code, :item_cost_tax_code, :time_tax_code
+
 
       attr_reader   :internal_id
       attr_accessor :external_id
